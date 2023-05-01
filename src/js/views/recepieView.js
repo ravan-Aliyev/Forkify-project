dc0e372ac9ccde7cf24c1269caaf7d7752317968
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-05-01 19:40:22 +0400

    fixed

--- src/js/views/recepieView.js
+++ src/js/views/recepieView.js
@@ -1,7 +1,7 @@
 import View from './View.js';
 
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-import { fracty } from 'fracty';
+import fracty from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
